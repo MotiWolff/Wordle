c0e7a17cb75dcf4ef3298ae2f2ac62270dd23bcf
MotiWolff <motiwolff@gmail.com>
2025-10-28 00:13:00 +0200

    Setup basic Wordle game UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { useState } from "react";
+import { boardDefault } from "./services/Words";
+import { AppContext } from "./contexts/AppContext";
 
 function App() {
-  return <></>;
+  const [board, setBoard] = useState(boardDefault);
+
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
+    </div>
+  );
 }
 
 export default App;
