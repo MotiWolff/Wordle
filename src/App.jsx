05a88525f4f042e63e57614db7eafe54af69c246
MotiWolff <motiwolff@gmail.com>
2025-10-28 11:41:01 +0200

    Feat: Implement basic game logic and context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
-import { useState } from "react";
+import { useMemo, useState, useCallback } from "react";
 import { boardDefault } from "./services/Words";
 import { AppContext } from "./contexts/AppContext";
 
@@ -9,18 +9,23 @@ function App() {
   const [board, setBoard] = useState(boardDefault);
   const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
 
-  function onSelectLetter(keyVal) {
-    if (currAttempt.letterPos > 4) return;
-    const newBoard = [...board];
-    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
-    setBoard(newBoard);
-    setCurrAttempt({
-      ...currAttempt,
-      letterPos: currAttempt.letterPos + 1,
-    });
-  }
+  const correctWord = "RIGHT";
 
-  function onDelete() {
+  const onSelectLetter = useCallback(
+    (keyVal) => {
+      if (currAttempt.letterPos > 4) return;
+      const newBoard = [...board];
+      newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+      setBoard(newBoard);
+      setCurrAttempt({
+        ...currAttempt,
+        letterPos: currAttempt.letterPos + 1,
+      });
+    },
+    [board, currAttempt]
+  );
+
+  const onDelete = useCallback(() => {
     if (currAttempt.letterPos === 0) return;
     const newBoard = [...board];
     newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
@@ -29,21 +34,33 @@ function App() {
       ...currAttempt,
       letterPos: currAttempt.letterPos - 1,
     });
-  }
+  }, [board, currAttempt]);
 
-  function onEnter() {
+  const onEnter = useCallback(() => {
     if (currAttempt.letterPos !== 5) return;
     setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
-  }
+  }, [currAttempt]);
+
+  const contextValue = useMemo(
+    () => ({
+      board,
+      setBoard,
+      currAttempt,
+      setCurrAttempt,
+      onDelete,
+      onSelectLetter,
+      onEnter,
+      correctWord,
+    }),
+    [board, currAttempt, onDelete, onSelectLetter, onEnter, correctWord]
+  );
 
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider
-        value={{ board, setBoard, currAttempt, setCurrAttempt, onDelete, onSelectLetter, onEnter }}
-      >
+      <AppContext.Provider value={contextValue}>
         <div className="game">
           <Board />
           <Keyboard />
