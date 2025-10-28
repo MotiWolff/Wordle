36bf298fbcd3274eb278af38efd0538263761013
MotiWolff <motiwolff@gmail.com>
2025-10-28 11:05:55 +0200

    Implement basic Wordle board and keyboard components

---
+++ src/components/Key.jsx
@@ -0,0 +1,23 @@
+import BackspaceIcon from "@mui/icons-material/Backspace";
+import { AppContext } from "../contexts/AppContext";
+import { useContext } from "react";
+export default function Key({ keyVal, bigKey, deleteKey }) {
+  const id = bigKey ? "big" : undefined;
+  const className = `key${deleteKey ? " delete" : ""}`;
+  const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
+
+  function selectLetter() {
+    if (keyVal === "ENTER") {
+      onEnter();
+    } else if (deleteKey) {
+      onDelete();
+    } else {
+      onSelectLetter(keyVal);
+    }
+  }
+  return (
+    <div className={className} id={id} onClick={selectLetter}>
+      {deleteKey ? <BackspaceIcon fontSize="inherit" /> : keyVal}
+    </div>
+  );
+}
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import React, { useCallback, useEffect, useContext, useMemo } from "react";
+import Key from "./Key";
+import { AppContext } from "../contexts/AppContext";
 
 export default function Keyboard() {
+  const keys1 = useMemo(
+    () => ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    []
+  );
+  const keys2 = useMemo(
+    () => ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    []
+  );
+  const keys3 = useMemo(() => ["Z", "X", "C", "V", "B", "N", "M"], []);
+  const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
+
+  const handleKeyboard = useCallback(
+    (event) => {
+      const pressed = event.key.toUpperCase();
+      if (pressed === "ENTER") {
+        onEnter();
+        return;
+      }
+      if (pressed === "BACKSPACE") {
+        onDelete();
+        return;
+      }
+      for (const k of keys1) {
+        if (pressed === k) {
+          onSelectLetter(k);
+          return;
+        }
+      }
+      for (const k of keys2) {
+        if (pressed === k) {
+          onSelectLetter(k);
+          return;
+        }
+      }
+      for (const k of keys3) {
+        if (pressed === k) {
+          onSelectLetter(k);
+          return;
+        }
+      }
+    },
+    [onEnter, onDelete, onSelectLetter, keys1, keys2, keys3]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
   return (
-    <div>Keyboard</div>
-  )
+    <div className="keyboard">
+      <div className="line1">
+        {keys1.map((key) => {
+          return <Key key={key} keyVal={key} />;
+        })}
+      </div>
+      <div className="line2">
+        {keys2.map((key) => {
+          return <Key key={key} keyVal={key} />;
+        })}
+      </div>
+      <div className="line3">
+        <Key keyVal={"ENTER"} bigKey />
+        {keys3.map((key) => {
+          return <Key key={key} keyVal={key} />;
+        })}
+        <Key keyVal={"⌫"} bigKey deleteKey />
+      </div>
+    </div>
+  );
 }
