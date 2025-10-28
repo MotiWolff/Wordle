bd2657caaea2b5991d714f1e487893ea27b4a6b3
MotiWolff <motiwolff@gmail.com>
2025-10-28 16:10:20 +0200

    Fetch and set the correct word from word list

--- src/services/Words.js
+++ src/services/Words.js
@@ -10,15 +10,17 @@ export const boardDefault = [
 
 export const generateWordSet = async () => {
   let wordSet;
+  let todaysWord;
   await fetch(wordBank).then((response) =>
     response.text().then((results) => {
       const wordArray = results
         .split(/\r?\n/) // handle Windows/Mac line endings
         .map((w) => w.trim())
         .filter(Boolean);
+      todaysWord = wordArray[Math.floor(Math.random() * wordArray.length)];
       wordSet = new Set(wordArray);
     })
   );
 
-  return { wordSet };
+  return { wordSet, todaysWord };
 };
