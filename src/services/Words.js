82d172f552d9892b095bf657a708d07153c0e598
MotiWolff <motiwolff@gmail.com>
2025-10-28 15:11:28 +0200

    feat: Fetch word list from wordle-bank.txt

--- src/services/Words.js
+++ src/services/Words.js
@@ -1,3 +1,4 @@
+import wordBank from "../wordle-bank.txt";
 export const boardDefault = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
@@ -6,3 +7,18 @@ export const boardDefault = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
 ];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  await fetch(wordBank).then((response) =>
+    response.text().then((results) => {
+      const wordArray = results
+        .split(/\r?\n/) // handle Windows/Mac line endings
+        .map((w) => w.trim())
+        .filter(Boolean);
+      wordSet = new Set(wordArray);
+    })
+  );
+
+  return { wordSet };
+};
