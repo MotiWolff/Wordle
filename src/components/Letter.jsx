5f296c1a679dc346edf9f3d3463b6bdec732c70b
MotiWolff <motiwolff@gmail.com>
2025-10-28 11:41:01 +0200

    Feat: Implement letter coloring based on word correctness

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -2,7 +2,23 @@ import React, { useContext } from "react";
 import { AppContext } from "../contexts/AppContext";
 
 export default function Letter({ letterPos, attemptVal }) {
-  const { board } = useContext(AppContext);
+  const { board, correctWord, currAttempt } = useContext(AppContext);
   const letter = board[attemptVal][letterPos];
-  return <div className="letter">{letter}</div>;
+
+  const correct = correctWord[letterPos] === letter;
+  const almost = !correct && letter !== "" && correctWord.includes(letter);
+
+  const letterState =
+    currAttempt.attempt > attemptVal
+      ? correct
+        ? "correct"
+        : almost
+          ? "almost"
+          : "error"
+      : undefined;
+  return (
+    <div className="letter" id={letterState}>
+      {letter}
+    </div>
+  );
 }
